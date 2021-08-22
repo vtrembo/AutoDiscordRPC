219175735938e6fb33bd7e9069e7a01ae2d31f47
Valerii <sweazy@pjwstk.edu.pl>
2021-08-23 01:18:57 +0200

    Slightly structured

--- AutoDiscordRPC/App.xaml.cs
+++ AutoDiscordRPC/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,36 @@ namespace AutoDiscordRPC
     /// </summary>
     public partial class App : Application
     {
+/*        public App()
+        {
+            _notifyIcon = new Forms.NotifyIcon();
+        }*/
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+
+
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            Thread t = new Thread(checkForProcess);
+            t.IsBackground = true;
+            t.Start();
+            base.OnStartup(e);
+        }
+        private void checkForProcess()
+        {
+            while (true)
+            {
+                bool isRunning = Process.GetProcessesByName("HotA_launcher")
+                    .FirstOrDefault(p => p.MainModule.FileName.StartsWith(@"C:\Sweazy\FUN\Heroes 3\Hota")) != default(Process);
+                if (isRunning)
+                {
+                    Process process = Process.Start("cmd.exe");
+                }
+                Thread.Sleep(1000);
+            }
+
+        }
     }
 }
