7098930ed14daa2304dbd8096e7c43b7f6b656c4
Valerii <sweazy@pjwstk.edu.pl>
2021-08-28 17:18:07 +0200

    updated createview

--- obj/Debug/net5.0-windows/App.g.i.cs
+++ obj/Debug/net5.0-windows/App.g.i.cs
@@ -56,7 +56,7 @@ namespace AutoDiscordRPC {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AutoDiscordRPC;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AutoDiscordRPC;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- obj/Debug/net5.0-windows/MVVM/View/HomeView.g.i.cs
+++ obj/Debug/net5.0-windows/MVVM/View/HomeView.g.i.cs
@@ -54,7 +54,7 @@ namespace AutoDiscordRPC.MVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AutoDiscordRPC;component/mvvm/view/homeview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AutoDiscordRPC;V1.0.0.0;component/mvvm/view/homeview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\MVVM\View\HomeView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
